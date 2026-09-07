1b1aeda11b81b191670e06a7e9e0dc09bbd63b13
steveasterafovo <steveasterafovo@gmail.com>
2026-09-07 10:10:31 +0100

    b

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -25,10 +25,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { name: "twitter:site", content: "@Lovable" },
     ],
     links: [
-      {
-        rel: "stylesheet",
-        href: appCss,
-      },
+      { rel: "stylesheet", href: appCss, },
       { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
