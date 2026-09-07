89b4131591e0c117e396c904ad152480e3498daf
steveasterafovo <steveasterafovo@gmail.com>
2026-09-07 03:58:08 +0100

    s

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Outlet, Link, createRootRouteWithContext, useRouter, HeadContent, Scripts } from "@tanstack/react-router";
-import { useEffect, type ReactNode } from "react";
-import { Toaster } from "@/components/ui/sonner";
+import { Outlet, createRootRouteWithContext, HeadContent, Scripts } from "@tanstack/react-router";
+import { type ReactNode } from "react";
+import { Toaster } from "sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import appCss from "../styles.css?url";
-import { reportLovableError } from "../lib/lovable-error-reporting";
+import { SITE } from "@/data/site";
+import { NotFoundComponent, ErrorComponent } from "@/components/errors";
+
 
 function NotFoundComponent() {
   return (
