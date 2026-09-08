f52e8a90c5c3d5f447e8ad4980617930b09948d1
steveasterafovo <steveasterafovo@gmail.com>
2026-09-08 16:25:23 +0100

    s

---
+++ src/components/site/BriefShell.tsx
@@ -0,0 +1,12 @@
+import type { ReactNode } from "react";
+import { BriefHeader, BriefFooter } from ".";
+
+export function BriefShell({ children }: { children: ReactNode }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <BriefHeader />
+      <main className="flex-1 pt-16 md:pt-20">{children}</main>
+      <BriefFooter />
+    </div>
+  );
+}
