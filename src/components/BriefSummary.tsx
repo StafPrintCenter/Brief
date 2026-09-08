34856dac2c0825e27895d0edad398056afb6c2fc
steveasterafovo <steveasterafovo@gmail.com>
2026-09-08 17:12:56 +0100

    q

--- src/components/BriefSummary.tsx
+++ src/components/BriefSummary.tsx
@@ -1,4 +1,5 @@
 import { deadlineLabel, logoLabel, projectTypeLabel, styleLabel, type BriefData } from "@/lib/briefStorage";
+import { SITE, SITE_LINK } from "@/data/site";
 
 function Row({ label, value }: { label: string; value: string }) {
   return (
