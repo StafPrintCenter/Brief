f3ba70413b25bd0a8016225cf9ad83f59bc72274
steveasterafovo <steveasterafovo@gmail.com>
2026-09-08 16:56:07 +0100

    z

--- src/components/pages/history/DeleteDialog.tsx
+++ src/components/pages/history/DeleteDialog.tsx
@@ -1,12 +1,5 @@
 import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { type BriefData } from "@/lib/briefStorage";
 
 interface DeleteBriefDialogProps {
