2be77bf11fb02d5513f52bd853e2be3d34bdcf6d
steveasterafovo <steveasterafovo@gmail.com>
2026-09-08 14:04:38 +0100

    s

--- src/components/pages/home/index.ts
+++ src/components/pages/home/index.ts
@@ -1,10 +1,4 @@
 export { HeroHeader } from "./HeroHeader";
 export { StepProgressBar } from "./StepProgressBar";
 export { CguDialog } from "./CguDialog";
-
-export { Step1Project } from "./steps/Step1Project";
-export { Step2Company } from "./steps/Step2Company";
-export { Step3Style } from "./steps/Step3Style";
-export { Step4Technical } from "./steps/Step4Technical";
-export { Step5Logistics } from "./steps/Step5Logistics";
-export { Step6Validation } from "./steps/Step6Validation";
+export * from "./steps";
