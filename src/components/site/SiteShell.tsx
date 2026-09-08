d205ccf855074a284048576017ca5dc0015e8287
steveasterafovo <steveasterafovo@gmail.com>
2026-09-08 16:25:08 +0100

    s

--- src/components/site/SiteShell.tsx
+++ src/components/site/SiteShell.tsx
@@ -1,12 +1,12 @@
 import type { ReactNode } from "react";
-import { SiteHeader, SiteFooter } from "./";
+import { BriefHeader, BriefFooter } from "./";
 
-export function SiteShell({ children }: { children: ReactNode }) {
+export function BriefShell({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col">
-      <SiteHeader />
+      <BriefHeader />
       <main className="flex-1 pt-16 md:pt-20">{children}</main>
-      <SiteFooter />
+      <BriefFooter />
     </div>
   );
 }
