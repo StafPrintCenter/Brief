90760015e4ac74475be5a04a494ff89a67c367a8
steveasterafovo <steveasterafovo@gmail.com>
2026-09-08 17:31:29 +0100

    s

--- src/components/pages/home/steps/6Validation.tsx
+++ src/components/pages/home/steps/6Validation.tsx
@@ -1,6 +1,6 @@
 import { CheckCircle2 } from "lucide-react";
 import { StepHeading } from "@/components/site/Primitives";
-import { BriefSummary } from "@/components/BriefSummary";
+import { BriefSummary } from "@/components/site";
 import { Button } from "@/components/ui/button";
 import type { BriefData } from "@/lib/briefStorage";
 
