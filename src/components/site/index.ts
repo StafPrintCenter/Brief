fe8bce6082fc9e771b80744a1eee32911e74e9a3
steveasterafovo <steveasterafovo@gmail.com>
2026-09-08 14:13:48 +0100

    s

--- src/components/site/index.ts
+++ src/components/site/index.ts
@@ -1,4 +1,4 @@
 export { SiteHeader } from "./Header";
 export { SiteFooter } from "./Footer";
 export { SiteShell } from "./SiteShell";
-export * from "./Pprimitives";
+export * from "./Primitives";
