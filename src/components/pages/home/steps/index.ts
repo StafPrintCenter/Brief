38adbf44ca19a353de0cb677bc25a3a966fd3107
steveasterafovo <steveasterafovo@gmail.com>
2026-09-08 14:19:20 +0100

    s

--- src/components/pages/home/steps/index.ts
+++ src/components/pages/home/steps/index.ts
@@ -2,5 +2,5 @@ export { Step1Project } from "./1Project";
 export { Step2Company } from "./2Company";
 export { Step3Style } from "./3Style";
 export { Step4Technical } from "./4Technical";
-export { Step5Logistics } from "./Step5Logistics";
-export { Step6Validation } from "./Step6Validation";
+export { Step5Logistics } from "./5Logistics";
+export { Step6Validation } from "./6Validation";
