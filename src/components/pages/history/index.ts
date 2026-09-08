a02ee5ebb201eae5359c60658898c724ede8b27c
steveasterafovo <steveasterafovo@gmail.com>
2026-09-08 16:37:41 +0100

    s

--- src/components/pages/history/index.ts
+++ src/components/pages/history/index.ts
@@ -1,4 +1,4 @@
-export { BriefCard } from "./BriefCard";
+export { BriefCard } from "./Card";
 export { BriefList } from "./BriefList";
 export { DeleteBriefDialog } from "./DeleteBriefDialog";
 export { ShareBriefDialog } from "./ShareBriefDialog";
