edf853346d46835aa459c68731778fb9020091d1
steveasterafovo <steveasterafovo@gmail.com>
2026-09-13 22:14:05 +0100

    s

--- src/components/pages/home/index.ts
+++ src/components/pages/home/index.ts
@@ -1,5 +1,6 @@
 export { HeroHeader } from "./HeroHeader";
 export { StepProgressBar } from "./ProgressBar";
 export { StepNavigation } from "./Navigation";
+export { BriefPreviewIllustration } from "./BriefPreviewIllustration";
 export { CguDialog } from "./CguDialog";
 export * from "./steps";
